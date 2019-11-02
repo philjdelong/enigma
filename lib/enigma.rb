eabbdbbd3220522f168cb87abe16c1ccd0576902
Phil DeLong <philjdelong@gmail.com>
2019-11-02 14:02:17 -0600

    Write tests for offset and key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,24 +1,24 @@
 require 'date'
 
 class Enigma
-  attr_reader :char_list, :key
-    # :date
+  attr_reader :char_list, :char_key
 
   def initialize
     @char_list = ("a".."z").to_a << " "
-    # @date = Time.now
+    @char_key = ("a".."d").to_a << " "
+    @date_info = Time.now
   end
 
   def random_five_digit_number
     rand.to_s[2..6].to_i
   end
 
-  # def todays_date
-  #   day = @date.day.to_s.rjust(2, "0")
-  #   month = @date.month.to_s.rjust(2, "0")
-  #   year = @date.year.to_s.delete("20")
-  #   day+month+year
-  # end
+  def todays_date
+    day = @date_info.day.to_s.rjust(2, "0")
+    month = @date_info.month.to_s.rjust(2, "0")
+    year = @date_info.year.to_s.slice(2,3)
+    day+month+year
+  end
 
   def selected_date(day, month, year)
     day = day.to_s.rjust(2, "0")
@@ -27,12 +27,32 @@ class Enigma
     day+month+year
   end
 
-  def date_squared(date)
-    date = date.to_i
-    date**2
+  def date_squared(date_info)
+    date_info = date_info.to_i
+    date_info**2
   end
 
-  def last_four(date)
-    date_squared(date) % 10000
+  def last_four(date_info)
+    date_squared(date_info) % 10000
+  end
+
+  def offset_assignment(date_info)
+    digits = last_four(date_info).to_s.split('')
+    digits = digits.map do |digit|
+      digit.to_i
+    end
+    digits
+  end
+
+  def offset
+    offset_assignments.reduce({}) do |acc, assignment|
+  end
+
+  def key_assignment(date_info)
+    digits = @char_list.to_s.split('')
+  end
+
+  def key
+    #######
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,33 +12,71 @@ class EnigmaTest < Minitest::Test
 
   def test_it_initializes_with_char_list_and_key
     assert_equal 27, @enigma.char_list.count
+    assert_equal 5, @enigma.char_key.count
   end
 
-  def test_it_can_generate_random_five_digit_number
-    assert_instance_of Integer, @enigma.random_five_digit_number
-  end
 
-  def test_it_can_provide_selected_date_in_DDMMYY_format
+# for offset
+  def test_it_can_provide_selected_date_in_DDMMYY_format_for_offset
     assert_equal "012345", @enigma.selected_date(01,23,2045)
   end
 
-  def test_it_can_square_the_date
-    assert_equal 152399025, @enigma.date_squared("012345")
-  end
-
-  def test_it_can_take_last_four_digits
+  def test_it_can_provide_todays_date_in_DDMMYY_format_for_offset
     # skip
-    assert_equal 9025, @enigma.last_four("012345")
+    assert_equal "021119", @enigma.todays_date
   end
 
+  def test_it_can_square_the_date_for_offset
+    date = "012345"
+    assert_equal 152399025, @enigma.date_squared(date)
+  end
 
+  def test_it_can_take_last_four_digits_for_offset
+    date = "012345"
+    assert_equal 9025, @enigma.last_four(date)
+  end
 
+  def test_it_can_tell_us_each_offset_value_for_offset
+    date = "012345"
+    assert_equal [9, 0, 2, 5], @enigma.offset_assignments(date)
+  end
 
-
-  def test_it_can_provide_todays_date_in_DDMMYY_format
+  def test_it_can_tell_us_offset_for_offset
     skip
-    assert_equal "110219", @enigma.todays_date
+    expected = {
+      :a => 9,
+      :b => 0,
+      :c => 2,
+      :d => 5
+    }
+    assert_equal expected, @enigma.offset("012345")
   end
+
+
+# key class
+  def test_it_can_tell_us_each_key_value_fer_key
+    skip
+    assert_equal [9, 0, 2, 5], @enigma.offset_assignment("012345")
+  end
+
+  def test_it_can_tell_us_key_for_key
+    skip
+    # need stub for this one
+    expected = {
+      :a_key => Integer,
+      :b_key => Integer,
+      :c_key => Integer,
+      :d_key => Integer
+    }
+    assert_equal expected, @enigma.key
+  end
+
+
+
+
+
+
+
 
   def test_it_can_encrypt_a_message_with_set_key_and_date
     skip
