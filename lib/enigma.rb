7a14e601eea0242a6319a4e790a425720dbafe92
Phil DeLong <philjdelong@gmail.com>
2019-11-01 19:05:44 -0600

    Write tests for interaction pattern

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,6 @@
 class Enigma
 
   def initialize
-
+    #RANDOMIZER => rand.to_s[2..6].to_i
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,11 +4,53 @@ class EnigmaTest < Minitest::Test
 
   def setup
     @enigma = Enigma.new
-    # @decryptor = Decriptor.new
-    # @encryptor = Encryptor.new
   end
 
   def test_it_exists
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_can_encrypt_a_message
+    skip
+    encrypted = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+    assert_equal encrypted, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_encrypt_a_message
+    skip
+    encrypted = {
+        decryption: "hello world",
+        key: "02715",
+        date: "040895"
+      }
+    assert_equal encrypted, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
+  def test_it_can_encrypt_a_message_with_a_key_using_todays_date
+    skip
+    encrypted = {
+      #=> # encryption hash here
+    }
+    assert_equal encrypted, @enigma.encrypt("hello world", "02715")
+  end
+
+  def test_it_can_decrypt_a_message_with_a_key_unsing_todays_date
+    skip
+    decrypted = {
+      #=> # decryption hash here
+    }
+    assert_equal decrypted, @enigma.decrypt(encrypted[:encryption], "02715")
+  end
+
+  def test_it_can_encrypt_a_message_generating_random_key_using_todays_date
+    skip
+    encrypted = {
+      #=> # encryption hash here
+    }
+    assert_equal encrypted, @enigma.encrypt("hello world")
   end
 end
