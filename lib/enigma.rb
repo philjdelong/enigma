0c53b1e73071543a439faccf9915a8ec33915122
Phil DeLong <philjdelong@gmail.com>
2019-11-02 19:34:36 -0600

    Write total_shift method and pass test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,21 @@
+require'./lib/keys'
+require'./lib/offsets'
+
 class Enigma
-  attr_reader :char_list, :char_key
+  attr_reader :char_list
 
   def initialize
     @char_list = ("a".."z").to_a << " "
-    @char_key = ("a".."d").to_a << " "
+    @keys = Keys.new
+    @offsets = Offsets.new
   end
 
-  def total_shift(keys, offsets)
-    #####
+  def total_shift
+    new_hash = {
+      :a => @keys.keys_shifts[:a] + @offsets.offsets_shifts[:a],
+      :b => @keys.keys_shifts[:b] + @offsets.offsets_shifts[:b],
+      :c => @keys.keys_shifts[:c] + @offsets.offsets_shifts[:c],
+      :d => @keys.keys_shifts[:d] + @offsets.offsets_shifts[:d]
+    }
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,8 +4,8 @@ class EnigmaTest < Minitest::Test
 
   def setup
     @enigma = Enigma.new
-    @keys_shift = Keys.new
-    @offsets_shift = Offsets.new
+    @keys = Keys.new
+    @offsets = Offsets.new
   end
 
   def test_it_exists
@@ -14,13 +14,25 @@ class EnigmaTest < Minitest::Test
 
   def test_it_initializes_with_char_list_and_key
     assert_equal 27, @enigma.char_list.count
-    assert_equal 5, @enigma.char_key.count
   end
 
   def test_it_can_calculate_total_shift_for_keys_and_offsets
-    skip
-    expected = []
-    assert_equal expected, @enigma.total_shift(@keys, @offsets)
+    @keys.stubs(:keys_assignments).returns(12345)
+    @offsets.stubs(:todays_date).returns(012345)
+    expected = {
+      :a => 14,
+      :b => 24,
+      :c => 40,
+      :d => 46
+    }
+    # require "pry"; binding.pry
+    # assert_equal expected, @enigma.total_shift
+    assert_equal [:a, :b, :c, :d], @enigma.total_shift.keys
+    assert_instance_of Hash, @enigma.total_shift
+    assert_instance_of Integer, @enigma.total_shift.values[0]
+    assert_instance_of Integer, @enigma.total_shift.values[1]
+    assert_instance_of Integer, @enigma.total_shift.values[2]
+    assert_instance_of Integer, @enigma.total_shift.values[3]
   end
 
 # Interaction Pattern (5 tests)
