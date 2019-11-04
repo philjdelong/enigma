dd60538ea0a4de07561b368560194467f5d05c93
Phil DeLong <philjdelong@gmail.com>
2019-11-04 07:26:01 -0700

    Rearrange to accommodate Message class removal

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -3,19 +3,13 @@ require_relative '../lib/test_helper'
 class EncryptorTest < Minitest::Test
 
   def setup
-    @encryptor_1 = Encryptor.new("hello world", "02715", "040895")
+    @encryptor = Encryptor.new("hello world", "02715", "040895")
     @key = Key.new
     @offset = Offset.new
   end
 
-  def test_it_can_encrypt_a_message_with_set_key_and_date
-    skip
-    encrypted = {
-        encryption: "keder ohulw",
-        key: "02715",
-        date: "040895"
-      }
-    assert_equal encrypted, @encryptor_1.encrypt("hello world", "02715", "040895")
+  def test_it_exists
+    assert_instance_of Encryptor, @encryptor
   end
 
   def test_it_can_calculate_total_shift_for_keys_and_offsets
@@ -25,22 +19,32 @@ class EncryptorTest < Minitest::Test
       :c => 73,
       :d => 20
     }
-    assert_equal expected, @encryptor_1.total_shift
-    assert_equal [:a, :b, :c, :d], @encryptor_1.total_shift.keys
+    assert_equal expected, @encryptor.total_shift
+    assert_equal [:a, :b, :c, :d], @encryptor.total_shift.keys
   end
 
   def test_it_can_tell_us_char_key_in_numeric_value
-    assert_equal 1, @encryptor_1.letter_key["a"]
-    assert_equal 13, @encryptor_1.letter_key["m"]
-    assert_equal 26, @encryptor_1.letter_key["z"]
+    assert_equal 1, @encryptor.letter_key["a"]
+    assert_equal 13, @encryptor.letter_key["m"]
+    assert_equal 26, @encryptor.letter_key["z"]
+  end
+
+  def test_it_can_split_letters_and_set_them_to_downcase
+    assert_equal ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"], @encryptor.split_letters_downcase("hello world")
   end
 
   def test_it_can_convert_message_to_numberic_value
     skip
-    assert_equal ["hello"], @encryptor_1.let_to_num("hello world")
+    assert_equal ["hello"], @encryptor.let_to_num("hello world")
   end
 
-  def test_it_can_split_letters_and_set_them_to_downcase
-    assert_equal ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"], @encryptor_1.split_letters_downcase("hello world")
+  def test_it_can_encrypt_a_message_with_set_key_and_date
+    skip
+    encrypted = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+    assert_equal encrypted, @encryptor.encrypt("hello world", "02715", "040895")
   end
 end
