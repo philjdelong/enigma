2782710ed2e036d5095a670afe9c465a3b9fff35
Phil DeLong <philjdelong@gmail.com>
2019-11-05 21:41:54 -0700

    Write methods for decryptor and pass tests

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -7,4 +7,56 @@ class Decryptor
     @shifter = Shifter.new(key, date)
     @char_list = ("a".."z").to_a << " "
   end
+
+  def split_letters_downcase(cyphertext)
+    decryption_split = cyphertext.split('')
+    decryption_split.map do |letter|
+      letter.downcase
+    end
+  end
+
+  def letter_keys
+    sum = 0
+    new_hash = {}
+    @char_list.map do |letter|
+      new_hash[letter] = sum + 1
+      sum = sum + 1
+    end
+  new_hash
+  end
+
+  def message_to_num(message)
+    split_letters_downcase(message).map do |letter|
+      if @char_list.include?(letter) == false
+        letter
+      elsif letter == (' ')
+        letter = 27
+      else
+        letter.ord - 96
+      end
+    end
+  end
+
+  def decryption(message)
+    unshift_message(message).map do |value|
+      if value %27 == 0
+        value = (' ')
+      elsif value.class == Integer
+        value = (value %27) + 96
+      else
+        value = value
+      end.chr
+    end.join
+  end
+
+  def unshift_message(message)
+    rotator = @shifter.total_shift.values
+    message_to_num(message).map do |value|
+      if value.class == Integer
+        value = (value - rotator.first)
+        rotator = rotator.rotate
+      end
+      value
+    end
+  end
 end
--- test/decryptor_test.rb
+++ test/decryptor_test.rb
@@ -11,6 +11,37 @@ class DecryptorTest < Minitest::Test
   end
 
   def test_it_can_convert_values_into_strings
+    skip
+    expected = "hello world!"
+    assert_equal expected, @decryptor.decryption("keder ohulw!")
+  end
+
+  # shiftable
+  def test_it_can_split_letters_and_set_them_to_downcase
+    assert_equal ["k", "e", "d", "e", "r", " ", "o", "h", "u", "l", "w", "!"], @decryptor.split_letters_downcase("keder ohulw!")
+  end
+
+  # shiftable
+  def test_it_can_tell_us_char_keys_in_numeric_value
+    assert_equal 1, @decryptor.letter_keys["a"]
+    assert_equal 13, @decryptor.letter_keys["m"]
+    assert_equal 26, @decryptor.letter_keys["z"]
+    assert_equal 27, @decryptor.letter_keys[' ']
+  end
+
+  # shiftable
+  def test_it_can_convert_message_to_numeric_value
+    expected = [11, 5, 4, 5, 18, 27, 15, 8, 21, 12, 23, "!"]
+    assert_equal expected, @decryptor.message_to_num("keder ohulw!")
+  end
+
+  # shiftable
+  def test_it_can_unshift_numeric_message
+    expected = [8, -22, -69, -15, 15, 0, -58, -12, 18, -15, -50, "!"]
+    assert_equal expected, @decryptor.unshift_message("keder ohulw!")
+  end
+
+  def test_it_can_convert_values_into_strings
     expected = "hello world!"
     assert_equal expected, @decryptor.decryption("keder ohulw!")
   end
